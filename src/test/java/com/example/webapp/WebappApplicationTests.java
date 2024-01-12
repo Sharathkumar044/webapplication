0dbd5f961cf932319c5a533cc443d4a92aac722d
Sharathkumar044 <sharathkumar044@gmail.com>
2024-01-12 13:23:23 +0530

    Your commit message here

--- src/test/java/com/example/webapp/WebappApplicationTests.java
+++ src/test/java/com/example/webapp/WebappApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.webapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class WebappApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private YourServiceClass yourService;
 
+    @Test
+    void contextLoads() {
+        // Basic test to check that the application context loads successfully
+        assertNotNull(yourService);
+    }
 }
